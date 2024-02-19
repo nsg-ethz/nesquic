6ae474eb80091ceda5452074e5fb6dd5e190604f
Laurin Brandner <laurinb@ethz.ch>
2024-02-19 08:58:36 +0100

    nesquic: init

---
+++ iuts/nesquic/src/bin/server.rs
@@ -0,0 +1,407 @@
+use std::{
+    collections::HashMap,
+    net::SocketAddr,
+    time::Duration
+};
+use common::{
+    args::ServerArgs, 
+    perf::Blob,
+    perf::process_req,
+};
+
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use log::{
+    info,
+    error,
+    debug
+};
+use quiche::Connection;
+use ring::rand::*;
+
+struct Client {
+    conn: quiche::Connection,
+    partial_responses: HashMap<u64, Blob>,
+}
+
+type ClientMap = HashMap<quiche::ConnectionId<'static>, Client>;
+
+const MAX_DATAGRAM_SIZE: usize = 1350;
+
+fn run(args: ServerArgs) -> Result<()> {
+    let mut socket = mio::net::UdpSocket::bind(args.listen).unwrap();
+
+    let mut poll = mio::Poll::new().unwrap();
+    let mut events = mio::Events::with_capacity(1024);
+    poll.registry()
+        .register(&mut socket, mio::Token(0), mio::Interest::READABLE)
+        .unwrap();
+
+    let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION).unwrap();
+
+    config.load_cert_chain_from_pem_file(&args.cert).unwrap();
+    config.load_priv_key_from_pem_file(&args.key).unwrap();
+    config.set_application_protos(&[b"perf"])?; 
+    config.set_max_idle_timeout(5000);
+    config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
+    config.set_initial_max_data(1_000_000_000);
+    config.set_initial_max_stream_data_bidi_local(1_000_000_000);
+    config.set_initial_max_stream_data_bidi_remote(1_000_000_000);
+    config.set_initial_max_stream_data_uni(1_000_000_000);
+    config.set_initial_max_streams_bidi(1);
+    config.set_disable_active_migration(true);
+    config.enable_early_data();
+
+    let rng = SystemRandom::new();
+    let conn_id_seed =
+        ring::hmac::Key::generate(ring::hmac::HMAC_SHA256, &rng).unwrap();
+
+    let mut clients = ClientMap::new();
+
+    let local_addr = socket.local_addr().unwrap();
+    let mut buf = [0; 65535];
+    let mut out = [0; MAX_DATAGRAM_SIZE];
+
+    loop {
+        poll.poll(&mut events, Some(Duration::from_secs(0))).unwrap();
+
+        'read: loop {
+            // If the event loop reported no events, it means that the timeout
+            // has expired, so handle it without attempting to read packets. We
+            // will then proceed with the send loop.
+            if events.is_empty() {
+                clients.values_mut().for_each(|c| c.conn.on_timeout());
+
+                break 'read;
+            }
+
+            let (len, from) = match socket.recv_from(&mut buf) {
+                Ok(v) => v,
+
+                Err(e) => {
+                    // There are no more UDP packets to read, so end the read
+                    // loop.
+                    if e.kind() == std::io::ErrorKind::WouldBlock {
+                        debug!("recv() would block");
+                        break 'read;
+                    }
+
+                    panic!("recv() failed: {:?}", e);
+                },
+            };
+
+            debug!("got {} bytes", len);
+
+            let pkt_buf = &mut buf[..len];
+
+            // Parse the QUIC packet's header.
+            let hdr = match quiche::Header::from_slice(
+                pkt_buf,
+                quiche::MAX_CONN_ID_LEN,
+            ) {
+                Ok(v) => v,
+
+                Err(e) => {
+                    error!("Parsing packet header failed: {:?}", e);
+                    continue 'read;
+                },
+            };
+
+            debug!("got packet {:?}", hdr);
+
+            let conn_id = ring::hmac::sign(&conn_id_seed, &hdr.dcid);
+            let conn_id = &conn_id.as_ref()[..quiche::MAX_CONN_ID_LEN];
+            let conn_id = conn_id.to_vec().into();
+
+            let client = if !clients.contains_key(&hdr.dcid) &&
+                !clients.contains_key(&conn_id)
+            {
+                if hdr.ty != quiche::Type::Initial {
+                    error!("Packet is not Initial");
+                    continue 'read;
+                }
+
+                if !quiche::version_is_supported(hdr.version) {
+                    info!("Doing version negotiation");
+
+                    let len =
+                        quiche::negotiate_version(&hdr.scid, &hdr.dcid, &mut out)
+                            .unwrap();
+
+                    let out = &out[..len];
+
+                    if let Err(e) = socket.send_to(out, from) {
+                        if e.kind() == std::io::ErrorKind::WouldBlock {
+                            debug!("send() would block");
+                            break;
+                        }
+
+                        panic!("send() failed: {:?}", e);
+                    }
+                    continue 'read;
+                }
+
+                let mut scid = [0; quiche::MAX_CONN_ID_LEN];
+                scid.copy_from_slice(&conn_id);
+
+                let scid = quiche::ConnectionId::from_ref(&scid);
+
+                // Token is always present in Initial packets.
+                let token = hdr.token.as_ref().unwrap();
+
+                // Do stateless retry if the client didn't send a token.
+                if token.is_empty() {
+                    info!("Doing stateless retry");
+
+                    let new_token = mint_token(&hdr, &from);
+
+                    let len = quiche::retry(
+                        &hdr.scid,
+                        &hdr.dcid,
+                        &scid,
+                        &new_token,
+                        hdr.version,
+                        &mut out,
+                    )
+                    .unwrap();
+
+                    let out = &out[..len];
+
+                    if let Err(e) = socket.send_to(out, from) {
+                        if e.kind() == std::io::ErrorKind::WouldBlock {
+                            debug!("send() would block");
+                            break;
+                        }
+
+                        panic!("send() failed: {:?}", e);
+                    }
+                    continue 'read;
+                }
+
+                let odcid = validate_token(&from, token);
+
+                // The token was not valid, meaning the retry failed, so
+                // drop the packet.
+                if odcid.is_none() {
+                    error!("Invalid address validation token");
+                    continue 'read;
+                }
+
+                if scid.len() != hdr.dcid.len() {
+                    error!("Invalid destination connection ID");
+                    continue 'read;
+                }
+
+                // Reuse the source connection ID we sent in the Retry packet,
+                // instead of changing it again.
+                let scid = hdr.dcid.clone();
+
+                info!("New connection: dcid={:?} scid={:?}", hdr.dcid, scid);
+
+                let conn = quiche::accept(
+                    &scid,
+                    odcid.as_ref(),
+                    local_addr,
+                    from,
+                    &mut config,
+                )
+                .unwrap();
+
+                let client = Client {
+                    conn,
+                    partial_responses: HashMap::new()
+                };
+
+                clients.insert(scid.clone(), client);
+                clients.get_mut(&scid).unwrap()
+            } else {
+                match clients.get_mut(&hdr.dcid) {
+                    Some(v) => v,
+
+                    None => clients.get_mut(&conn_id).unwrap(),
+                }
+            };
+
+            let recv_info = quiche::RecvInfo {
+                to: socket.local_addr().unwrap(),
+                from,
+            };
+
+            // Process potentially coalesced packets.
+            let read = match client.conn.recv(pkt_buf, recv_info) {
+                Ok(v) => v,
+
+                Err(e) => {
+                    error!("{} recv failed: {:?}", client.conn.trace_id(), e);
+                    continue 'read;
+                },
+            };
+
+            info!("{} processed {} bytes", client.conn.trace_id(), read);
+
+            while let Ok((read, fin)) = client.conn.stream_recv(0, pkt_buf) {
+                if fin {
+                    let req = &pkt_buf[..read];
+                    handle_request(req, client)?;
+                }
+            }
+
+            // continue partial responses
+            for stream_id in client.conn.writable() {
+                handle_writable_stream(client, stream_id)?;
+            }
+        }
+
+        for client in clients.values_mut() {
+            loop {
+                let (write, send_info) = match client.conn.send(&mut out) {
+                    Ok(v) => v,
+
+                    Err(quiche::Error::Done) => {
+                        break;
+                    },
+
+                    Err(e) => {
+                        error!("{} send failed: {:?}", client.conn.trace_id(), e);
+
+                        client.conn.close(false, 0x1, b"fail").ok();
+                        break;
+                    },
+                };
+
+                if let Err(e) = socket.send_to(&out[..write], send_info.to) {
+                    if e.kind() == std::io::ErrorKind::WouldBlock {
+                        break;
+                    }
+
+                    panic!("send() failed: {:?}", e);
+                }
+            }
+        }
+
+        // Garbage collect closed connections.
+        clients.retain(|_, ref mut c| {
+            if c.conn.is_closed() {
+                info!(
+                    "{} connection collected {:?}",
+                    c.conn.trace_id(),
+                    c.conn.stats()
+                );
+            }
+
+            !c.conn.is_closed()
+        });
+    }
+}
+
+fn handle_writable_stream(client: &mut Client, stream_id: u64) -> Result<()> {
+    let blob = client.partial_responses
+        .get_mut(&stream_id)
+        .ok_or_else(|| anyhow!("unknown stream"))?;
+
+    send_blob(&mut client.conn, stream_id, blob)?;
+
+    if blob.cursor >= blob.size {
+        client.partial_responses.remove(&stream_id);
+    }
+
+    Ok(())
+}
+
+fn handle_request(req: &[u8], client: &mut Client) -> Result<()> {
+    let mut blob = process_req(req)?;
+    let stream_id = client.partial_responses.len() as u64;
+    send_blob(&mut client.conn, stream_id, &mut blob)?;
+
+    client.partial_responses.insert(stream_id, blob);
+
+    Ok(())
+}
+
+fn send_blob(conn: &mut Connection, stream_id: u64, blob: &mut Blob) -> Result<()> {
+    let buf = vec![0; (blob.size - blob.cursor) as usize];
+    let sent = match conn.stream_send(stream_id, &buf, true) {
+        Ok(cnt) => {
+            debug!("sent some data: {cnt}/{}", buf.len());
+            Ok(cnt)
+        },
+        Err(quiche::Error::Done) => Ok(0),
+        Err(e) => Err(e),
+    }?;
+
+    blob.cursor += sent as u64;
+
+    Ok(())
+}
+
+/// Generate a stateless retry token.
+///
+/// The token includes the static string `"quiche"` followed by the IP address
+/// of the client and by the original destination connection ID generated by the
+/// client.
+///
+/// Note that this function is only an example and doesn't do any cryptographic
+/// authenticate of the token. *It should not be used in production system*.
+fn mint_token(hdr: &quiche::Header, src: &SocketAddr) -> Vec<u8> {
+    let mut token = Vec::new();
+
+    token.extend_from_slice(b"quiche");
+
+    let addr = match src.ip() {
+        std::net::IpAddr::V4(a) => a.octets().to_vec(),
+        std::net::IpAddr::V6(a) => a.octets().to_vec(),
+    };
+
+    token.extend_from_slice(&addr);
+    token.extend_from_slice(&hdr.dcid);
+
+    token
+}
+
+/// Validates a stateless retry token.
+///
+/// This checks that the ticket includes the `"quiche"` static string, and that
+/// the client IP address matches the address stored in the ticket.
+///
+/// Note that this function is only an example and doesn't do any cryptographic
+/// authenticate of the token. *It should not be used in production system*.
+fn validate_token<'a>(
+    src: &SocketAddr, token: &'a [u8],
+) -> Option<quiche::ConnectionId<'a>> {
+    if token.len() < 6 {
+        return None;
+    }
+
+    if &token[..6] != b"quiche" {
+        return None;
+    }
+
+    let token = &token[6..];
+
+    let addr = match src.ip() {
+        std::net::IpAddr::V4(a) => a.octets().to_vec(),
+        std::net::IpAddr::V6(a) => a.octets().to_vec(),
+    };
+
+    if token.len() < addr.len() || &token[..addr.len()] != addr.as_slice() {
+        return None;
+    }
+
+    Some(quiche::ConnectionId::from_ref(&token[addr.len()..]))
+}
+
+fn main() {
+    env_logger::init();
+
+    let args = ServerArgs::parse();
+    let code = {
+        if let Err(e) = run(args) {
+            error!("ERROR: {e}");
+            1
+        } else {
+            0
+        }
+    };
+    std::process::exit(code);
+}
