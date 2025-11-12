0352570a113b48538885a2607e5bc32ea2cb5d5d
Laurin Brandner <mail@laurinbrandner.ch>
2025-11-12 14:22:32 +0100

    qbench: add quiche support

--- qbench/src/main.rs
+++ qbench/src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand, ValueEnum};
+use quiche_iut::{Client as QuicheClient, Server as QuicheServer};
 use quinn_iut::{Client as QuinnClient, Server as QuinnServer};
 use tokio::signal::unix::{signal, SignalKind};
 use utils::bin::{Client, ClientArgs, Server, ServerArgs};
@@ -22,7 +23,7 @@ pub enum Command {
 #[derive(Parser, Debug)]
 pub struct ClientLibArgs {
     #[clap(short, long, value_enum)]
-    pub lib: Lib,
+    pub lib: Library,
 
     #[clap(flatten)]
     pub client_args: ClientArgs,
@@ -31,40 +32,48 @@ pub struct ClientLibArgs {
 #[derive(Parser, Debug)]
 pub struct ServerLibArgs {
     #[clap(short, long, value_enum)]
-    pub lib: Lib,
+    pub lib: Library,
 
     #[clap(flatten)]
     pub server_args: ServerArgs,
 }
 
 #[derive(Copy, Clone, Debug, ValueEnum)]
-pub enum Lib {
+pub enum Library {
     Quinn,
     Quiche,
     Msquic,
     Ngtcp,
 }
 
-async fn run_client(lib: Lib, args: ClientArgs) -> Result<()> {
-    let mut client = match lib {
-        Lib::Quinn => QuinnClient::new(args)?,
-        Lib::Quiche => unimplemented!("quiche"),
-        Lib::Msquic => unimplemented!("msquic"),
-        Lib::Ngtcp => unimplemented!("ngtcp"),
-    };
-
-    client.run().await
+async fn run_client(lib: Library, args: ClientArgs) -> Result<()> {
+    match lib {
+        Library::Quinn => {
+            let mut client = QuinnClient::new(args)?;
+            client.run().await
+        }
+        Library::Quiche => {
+            let mut client = QuicheClient::new(args)?;
+            client.run().await
+        }
+        Library::Msquic => unimplemented!("msquic"),
+        Library::Ngtcp => unimplemented!("ngtcp"),
+    }
 }
 
-async fn run_server(lib: Lib, args: ServerArgs) -> Result<()> {
-    let mut server = match lib {
-        Lib::Quinn => QuinnServer::new(args)?,
-        Lib::Quiche => unimplemented!("quiche"),
-        Lib::Msquic => unimplemented!("msquic"),
-        Lib::Ngtcp => unimplemented!("ngtcp"),
-    };
-
-    server.listen().await
+async fn run_server(lib: Library, args: ServerArgs) -> Result<()> {
+    match lib {
+        Library::Quinn => {
+            let mut server = QuinnServer::new(args)?;
+            server.listen().await
+        }
+        Library::Quiche => {
+            let mut server = QuicheServer::new(args)?;
+            server.listen().await
+        }
+        Library::Msquic => unimplemented!("msquic"),
+        Library::Ngtcp => unimplemented!("ngtcp"),
+    }
 }
 
 #[tokio::main]
--- qbench/tests/e2e.rs
+++ qbench/tests/e2e.rs
@@ -1,6 +1,8 @@
 use utils::bin::{Client, ClientArgs, Server, ServerArgs};
 
-pub async fn establish_connection<C: Client, S: Server + Send>() {
+pub async fn run<C: Client, S: Server + Send>() {
+    env_logger::init();
+
     tokio::spawn(async move {
         let mut server = S::new(ServerArgs::test()).expect("server::new");
         let res = server.listen().await;
@@ -9,16 +11,27 @@ pub async fn establish_connection<C: Client, S: Server + Send>() {
 
     let mut client = C::new(ClientArgs::test()).expect("client::new");
     let res = client.run().await;
+    println!("Client run result: {:?}", res);
     assert!(res.is_ok(), "{}", res.err().unwrap());
     assert!(client.stats().throughputs().mean() > 0.0);
 }
 
 #[tokio::test]
-async fn quinn_uploads_data_to_quinn() {
-    establish_connection::<quinn_iut::Client, quinn_iut::Server>().await;
+async fn run_quinn() {
+    run::<quinn_iut::Client, quinn_iut::Server>().await;
 }
 
 #[tokio::test]
-async fn quinn_uploads_data_to_quiche() {
-    establish_connection::<quinn_iut::Client, quiche_iut::Server>().await;
+async fn run_quiche() {
+    run::<quiche_iut::Client, quiche_iut::Server>().await;
+}
+
+#[tokio::test]
+async fn run_quinn_quiche() {
+    run::<quinn_iut::Client, quiche_iut::Server>().await;
+}
+
+#[tokio::test]
+async fn run_quiche_quinn() {
+    run::<quiche_iut::Client, quinn_iut::Server>().await;
 }
