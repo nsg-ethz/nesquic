9a8011380f24b1581f2f2bcbada812b0552a708b
Laurin Brandner <mail@laurinbrandner.ch>
2025-11-24 10:45:51 +0100

    nesquic: push metrics to gateway

--- nesquic/src/main.rs
+++ nesquic/src/main.rs
@@ -1,19 +1,24 @@
+use crate::metrics::*;
 use anyhow::Result;
 use clap::{Parser, Subcommand, ValueEnum};
 use msquic_iut::{Client as MsQuicClient, Server as MsQuicServer};
 use quiche_iut::{Client as QuicheClient, Server as QuicheServer};
 use quinn_iut::{Client as QuinnClient, Server as QuinnServer};
+use std::{collections::HashMap, env};
 use tokio::signal::unix::{signal, SignalKind};
+use tracing::{error, info};
 use utils::bin::{Client, ClientArgs, Server, ServerArgs};
 
-#[derive(Parser, Debug)]
+mod metrics;
+
+#[derive(Parser, Debug, Clone)]
 #[clap(author, version, about)]
 pub struct Cli {
     #[clap(subcommand)]
     pub command: Command,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand, Debug, Clone)]
 pub enum Command {
     /// Run as client
     Client(ClientLibArgs),
@@ -21,7 +26,7 @@ pub enum Command {
     Server(ServerLibArgs),
 }
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 pub struct ClientLibArgs {
     #[clap(short, long, value_enum)]
     pub lib: Library,
@@ -30,7 +35,7 @@ pub struct ClientLibArgs {
     pub client_args: ClientArgs,
 }
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 pub struct ServerLibArgs {
     #[clap(short, long, value_enum)]
     pub lib: Library,
@@ -83,22 +88,60 @@ async fn run_server(lib: Library, args: ServerArgs) -> Result<()> {
     }
 }
 
+async fn push_metrics(cli: Cli) -> Result<()> {
+    if let Ok(push_gateway) = env::var("PR_PUSH_GATEWAY") {
+        info!("Pushing metrics to {}", push_gateway);
+
+        let log_level = tracing::level_filters::LevelFilter::current().to_string();
+        let mode = match cli.command {
+            Command::Client(_) => "client",
+            Command::Server(_) => "server",
+        };
+        let library = match cli.command {
+            Command::Client(args) => args.lib.clone(),
+            Command::Server(args) => args.lib.clone(),
+        };
+        let library = format!("{:?}", library);
+
+        let mut labels = HashMap::new();
+        labels.insert("log_level", log_level.as_str());
+        labels.insert("library", library.as_str());
+        labels.insert("mode", mode);
+
+        metrics::push_all(push_gateway, &labels).await?;
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
         .init();
 
+    let cli = Cli::parse();
+    let term_cli = cli.clone();
     tokio::spawn(async move {
         let mut sigterm = signal(SignalKind::terminate()).unwrap();
         tokio::select! {
-            _ = tokio::signal::ctrl_c() => std::process::exit(0),
-            _ = sigterm.recv() => std::process::exit(0),
+            _ = tokio::signal::ctrl_c() => {
+                if let Err(e) = push_metrics(term_cli).await {
+                    error!("Error pushing metrics: {}", e);
+                }
+                std::process::exit(0);
+            },
+            _ = sigterm.recv() => {
+                if let Err(e) = push_metrics(term_cli).await {
+                    error!("Error pushing metrics: {}", e);
+                }
+                std::process::exit(0);
+            },
         }
     });
 
-    let cli = Cli::parse();
+    NESQUIC_RUNS.inc();
     match &cli.command {
         Command::Client(args) => {
             run_client(args.lib, args.client_args.clone())
@@ -111,4 +154,6 @@ async fn main() {
                 .unwrap();
         }
     }
+
+    Ok(())
 }
