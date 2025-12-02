51c6a1cd491e7fe9899aa7ff5f18e714cafb4c4b
Laurin Brandner <mail@laurinbrandner.ch>
2025-12-02 11:09:31 +0100

    grafana: sections

--- script/main.dashboard.py
+++ script/main.dashboard.py
@@ -4,17 +4,55 @@ from grafanalib.core import (
     BarChart,
     Dashboard,
     GridPos,
+    RowPanel,
     Target,
 )
 
 
-def io_targets(library):
+def io_panels(library):
+    num = BarChart(
+        title="I/O Syscalls",
+        dataSource="prometheus",
+        orientation="vertical",
+        targets=[
+            Target(
+                expr=f'io_syscalls_invocations_sum{{library="{library}"}}',
+                format="table",
+                instant=True,
+            )
+        ],
+        showLegend=False,
+        gridPos=GridPos(h=8, w=12, x=0, y=0),
+        xField="syscall",
+        axisLabel="Invocations",
+    )
+
+    vol = BarChart(
+        title="I/O Data Volume",
+        dataSource="prometheus",
+        orientation="vertical",
+        targets=[
+            Target(
+                expr=f'io_syscalls_data_volume_sum{{library="{library}"}}',
+                format="table",
+                instant=True,
+            )
+        ],
+        showLegend=False,
+        gridPos=GridPos(h=8, w=12, x=12, y=0),
+        xField="syscall",
+        axisLabel="Bytes",
+    )
+
+    return [vol, num]
+
+
+def library_panels(library):
     return [
-        Target(
-            expr=f'io_syscalls_data_volume_sum{{library="{library}"}}',
-            format="table",
-            instant=True,
-        )
+        RowPanel(
+            title=library.capitalize(),
+        ),
+        *io_panels(library),
     ]
 
 
@@ -22,15 +60,5 @@ dashboard = Dashboard(
     title="Nesquic",
     description="An overview dashboard for the Nesquic projecgt",
     timezone="browser",
-    panels=[
-        BarChart(
-            title="Quinn I/O",
-            dataSource="prometheus",
-            orientation="vertical",
-            targets=io_targets("quinn"),
-            showLegend=False,
-            gridPos=GridPos(h=8, w=16, x=0, y=0),
-            xField="syscall",
-        ),
-    ],
+    panels=library_panels("quinn"),
 ).auto_panel_ids()
