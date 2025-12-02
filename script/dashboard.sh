0c846bbaa953fd033de967e787ccb9c0fdeb7aeb
Laurin Brandner <mail@laurinbrandner.ch>
2025-12-02 14:40:42 +0100

    grafana: one dashboard per library

--- script/dashboard.sh
+++ script/dashboard.sh
@@ -2,5 +2,5 @@ WORKSPACE=$(dirname "$(readlink -f "$0")")/..
 
 set -e
 
-${WORKSPACE}/script/main.dashboard.py
+LIBRARY=quinn ${WORKSPACE}/script/main.dashboard.py
 docker compose -f ${WORKSPACE}/docker/backend.yml restart grafana
--- script/main.dashboard.py
+++ script/main.dashboard.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S uv tool run --from git+https://github.com/lbrndnr/grafanalib@main generate-dashboard -o docker/grafana/dashboard/main.json
 
+import os
+
 from grafanalib.core import (
     BarChart,
     Dashboard,
@@ -11,22 +13,37 @@ from grafanalib.core import (
 PANEL_HEIGHT = 8
 DASHBOARD_WIDTH = 24
 DASHBOARD_MID = DASHBOARD_WIDTH / 2
+Y = 0
 
 
-def io_panels(library, mode, ym):
+def y_offset():
+    global Y
+
+    res = Y
+    Y += PANEL_HEIGHT
+    return res
+
+
+def format_labels(labels):
+    return ", ".join(f"{k}='{v}'" for k, v in labels.items())
+
+
+def io_panels(**labels):
+    mode = labels["mode"]
+
     num = BarChart(
         title=f"{mode.capitalize()} I/O Syscalls",
         dataSource="prometheus",
         orientation="vertical",
         targets=[
             Target(
-                expr=f'io_syscalls_invocations_sum{{library="{library}", mode="{mode}"}}',
+                expr=f"io_syscalls_invocations_sum{{{format_labels(labels)}}}",
                 format="table",
                 instant=True,
             )
         ],
         showLegend=False,
-        gridPos=GridPos(h=PANEL_HEIGHT, w=DASHBOARD_MID, x=0, y=ym * PANEL_HEIGHT),
+        gridPos=GridPos(h=PANEL_HEIGHT, w=DASHBOARD_MID, x=0, y=y_offset()),
         xField="syscall",
         axisLabel="Invocations",
     )
@@ -37,15 +54,13 @@ def io_panels(library, mode, ym):
         orientation="vertical",
         targets=[
             Target(
-                expr=f'io_syscalls_data_volume_sum{{library="{library}", mode="{mode}"}}',
+                expr=f"io_syscalls_data_volume_sum{{{format_labels(labels)}}}",
                 format="table",
                 instant=True,
             )
         ],
         showLegend=False,
-        gridPos=GridPos(
-            h=PANEL_HEIGHT, w=DASHBOARD_MID, x=DASHBOARD_MID, y=ym * PANEL_HEIGHT
-        ),
+        gridPos=GridPos(h=PANEL_HEIGHT, w=DASHBOARD_MID, x=DASHBOARD_MID, y=y_offset()),
         xField="syscall",
         axisLabel="Bytes",
     )
@@ -53,39 +68,57 @@ def io_panels(library, mode, ym):
     return [vol, num]
 
 
-def throughput_panel(library, ym):
+def throughput_panel(**labels):
     return BarChart(
         title="Throughput",
         dataSource="prometheus",
         orientation="vertical",
         targets=[
             Target(
-                expr=f'throughput_sum{{library="{library}"}}',
+                expr=f"throughput_sum{{{format_labels(labels)}}}",
                 format="table",
                 instant=True,
             )
         ],
         showLegend=False,
-        gridPos=GridPos(h=PANEL_HEIGHT, w=DASHBOARD_WIDTH, x=0, y=ym * PANEL_HEIGHT),
-        xField="job",
+        gridPos=GridPos(h=PANEL_HEIGHT, w=DASHBOARD_WIDTH, x=0, y=y_offset()),
+        xField="exported_job",
         axisLabel="Mbps",
     )
 
 
-def library_panels(library):
+def overview_panels(**labels):
     return [
         RowPanel(
-            title=library.capitalize(),
+            title="Overview",
+            gridPos=GridPos(h=1, w=DASHBOARD_WIDTH, x=0, y=y_offset()),
         ),
-        *io_panels(library, "server", 0),
-        *io_panels(library, "client", 1),
-        throughput_panel(library, 2),
+        throughput_panel(**labels),
     ]
 
 
+def experiments_panels(title, **labels):
+    return [
+        RowPanel(
+            title=title,
+            gridPos=GridPos(h=1, w=DASHBOARD_WIDTH, x=0, y=y_offset()),
+        ),
+        *io_panels(mode="server", **labels),
+        *io_panels(mode="client", **labels),
+    ]
+
+
+library = os.environ.get("LIBRARY")
+if library is None:
+    raise ValueError("LIBRARY environment variable is not set")
+
 dashboard = Dashboard(
-    title="Nesquic",
-    description="An overview dashboard for the Nesquic projecgt",
+    title=library.capitalize(),
+    tags="nesquic",
     timezone="browser",
-    panels=library_panels("quinn"),
+    panels=[
+        *overview_panels(),
+        *experiments_panels("Unbounded", library=library, exported_job="test"),
+        *experiments_panels("50ms Delay", library=library, exported_job="delay50"),
+    ],
 ).auto_panel_ids()
