21e856d6cee3f99cc3ae005626e99338c7a072ed
Laurin Brandner <mail@laurinbrandner.ch>
2026-05-31 16:43:18 +0200

    ci: build container for stable release

--- iut/neqo/src/main.rs
+++ iut/neqo/src/main.rs
@@ -1,3 +1,6 @@
+use common::run;
+use neqo_iut::{Client, Server};
+
 mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
@@ -10,5 +13,16 @@ async fn main() -> anyhow::Result<()> {
         .map(|(_, v)| *v)
         .unwrap_or("unknown");
 
-    iut_common::run::<neqo_iut::Client, neqo_iut::Server>("neqo", version).await
+    run::<Client, Server>("neqo", version).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use common::test;
+
+    #[tokio::test]
+    async fn test_connectivity() {
+        test::connectivity::<Client, Server>().await;
+    }
 }
